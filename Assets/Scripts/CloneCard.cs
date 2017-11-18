0eaaaa65f64026d356f67f104344b5a9d8eb44d0
Mikhail Gurakov <gurakov_ma@tomsk.lemz.ru>
2017-11-18 23:20:36 +0700

    close issue33

---
+++ Assets/Scripts/CloneCard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneCard : MonoBehaviour 
+{
+	public GameObject button;
+	public Canvas canvas;
+	// Use this for initialization
+	void Start () 
+	{
+		GameObject newButton = Instantiate(button, button.transform);
+		// newButton.SetActive(true);
+		var buttonPosition = newButton.transform.position;
+		newButton.transform.position.Set(buttonPosition.x, buttonPosition.y + 50, buttonPosition.z);
+	}
+}

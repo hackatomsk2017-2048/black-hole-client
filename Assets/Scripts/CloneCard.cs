e7f3c1935670842e06bd562526cf7dcb638e2af9
Лизин Александр Сергеевич <lizin_as@tomsk.lemz.ru>
2017-11-19 02:24:19 +0700

    * Текущие наработки (Close #41)

---
+++ Assets/Scripts/CloneCard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloneCard : MonoBehaviour 
+{
+	public GameObject button;
+	public Canvas canvas;
+
+	private GameObject[][] buttons;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log ("wefwef");
+
+		buttons = new GameObject[5][];
+		for (int i = 0; i < 5; i++) {
+			buttons[i] = new GameObject[5];
+		}
+
+		Sprite[] cards = Resources.LoadAll<Sprite> ("cards");
+
+		for (int i = 0; i < 1; i++) {
+			Debug.Log ("1");
+				
+			for (int j = 0; j < 3; j++) {
+
+				GameObject newButton = Instantiate (button, button.transform);
+				newButton.GetComponent<Image> ().overrideSprite = cards [j];
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/EnterPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterPoint : MonoBehaviour
+{
+	public GameObject prefub;
+	public GameObject button;
+
+	void Start () 
+	{
+		Debug.Log ("wefkhwekfweknfj");
+		// for (int i = 0; i < 5; i++)
+		// {
+			button = Instantiate(prefub, prefub.transform);
+			button.SetActive(true);
+			button.transform.position = new Vector3(Screen.width/2, Screen.height/2, 0);
+		// }
+
+	}
+}

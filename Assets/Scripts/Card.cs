c9f6bbaf13dbce9471374ab4810a07b1693d6b61
Лизин Александр Сергеевич <lizin_as@tomsk.lemz.ru>
2017-11-19 11:59:20 +0700

    * Класс CloneCard.cs разбит на Desc.cs и Card.cs. (Close #66)

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour {
+
+	private int m_id;
+	private GameObject m_prefab;
+	private Transform m_transform;
+	private Vector3 m_position;
+	private Sprite[] m_cards;
+
+	public int id()
+	{
+		return m_id;
+	}
+
+	public Card(GameObject prefab, Transform transform, int id, Vector3 position, Sprite[] cards)
+	{
+		m_id = id;
+		m_position = position;
+		m_prefab = prefab;
+		m_transform = transform;
+		m_cards = cards;
+	}
+
+	// Use this for initialization
+	public void Start ()
+	{
+ 		GameObject button = Instantiate(m_prefab, m_transform);
+ 		button.transform.position = m_position;
+ 		button.transform.localScale = new Vector3(3f, 3f, 3f);
+ 		button.GetComponent<Image>().sprite = m_cards[Random.Range(0, m_cards.Length - 1)];
+		button.GetComponent<Button>().onClick.AddListener(OnClick);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnClick()
+	{
+		Debug.Log("You have clicked the button!");
+	}
+
+}
--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CloneCard : MonoBehaviour 
+public class Deck : MonoBehaviour
 {
 	public GameObject prefab;
 	public Canvas canvas;
-	
+
+	private Card [][] m_cards;
+
 	// Use this for initialization
 	void Start ()
 	{
-		Sprite[] cards = Resources.LoadAll<Sprite> ("cards");
-		// Canvas canvas = GameObject.FindWithTag("Canvas").GetComponent<Canvas>();
+		m_cards = new Card[5][];
+		for (int i = 0; i < 5; i++)
+		{
+			m_cards[i] = new Card[5];
+		}
+		
+		Sprite[] cardsImages = Resources.LoadAll<Sprite> ("cards");
 		float absWidth = canvas.GetComponent<RectTransform>().rect.width;
 		float oneAbsW = absWidth / 9; // единица масштабирования ширины - смотреть issue #49
 		float absHeight = canvas.GetComponent<RectTransform>().rect.height;
@@ -21,15 +28,21 @@ public class CloneCard : MonoBehaviour
 		{
 			for (int j = 0; j < 4; ++j)
 			{
-		 		GameObject button = Instantiate(prefab, canvas.transform);
-		 		button.name = "Card" + i.ToString() + j.ToString();
-		 		button.transform.position = new Vector3(-3f * oneAbsW + j * 2f * oneAbsW, 
-		 												0.5f * oneAbsH + i * 2f * oneAbsH, 
-		 												0f);
-		 		button.transform.localScale = new Vector3(3f, 3f, 3f);
-		 		button.GetComponent<Image>().sprite = cards[Random.Range(0, cards.Length - 1)];
+				Debug.Log("wqewef");
+				m_cards [i] [j] = new Card(prefab, canvas.transform, i*j, new Vector3(
+					-3f * oneAbsW + j * 2f * oneAbsW, 
+					0.5f * oneAbsH + i * 2f * oneAbsH, 
+					0f), cardsImages
+				);
+				m_cards [i] [j].Start();
 			}
 		}
 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
 	}
 }

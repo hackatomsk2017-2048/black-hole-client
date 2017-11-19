86a80944749c5b9e9dfc26f21ab583859b3339fe
Лизин Александр Сергеевич <lizin_as@tomsk.lemz.ru>
2017-11-19 16:28:01 +0700

    * Сделан начальный экран (close #70)

---
+++ Assets/Scripts/LogoSceneController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoSceneController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine(ToPrepeareScene());
+	}
+
+	public IEnumerator ToPrepeareScene()
+	{
+		yield return new WaitForSeconds(3); // waits 3 seconds
+		//trigger = true; // will make the update method pick up 
+		Debug.Log("wefwkuehfwkej");
+		Application.LoadLevel("PrepareScene");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

fce7b089cfad1feb40e6faa2d86c547dbc6eb1c3
Лизин Александр Сергеевич <lizin_as@tomsk.lemz.ru>
2017-11-19 14:31:56 +0700

    * Нажатие на карту приводит к ее перемещению (close #22)

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,48 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class Card : MonoBehaviour {
+	public enum CardType
+	{
+		PLUS_1 = 0,
+		PLUS_2 = 1,
+		PLUS_3 = 2,
+		PLUS_4 = 3,
+		PLUS_5 = 4,
+		PLUS_6 = 5,
+		PLUS_7 = 6,
+		PLUS_8 = 7,
+		PLUS_9 = 8
+	}
 
+	public static int cardTypeToInt(CardType cardType)
+	{
+		return (int) cardType;
+	}
+
+	private GameObject m_button;
 	private int m_id;
-	private GameObject m_prefab;
-	private Transform m_transform;
-	private Vector3 m_position;
-	private Sprite[] m_cards;
+	// private GameObject m_prefab;
+	// private Transform m_transform;
+	// private Vector3 m_position;
+	// private Sprite[] m_cards;
+	private CardType m_cardType;
 
 	public int id()
 	{
 		return m_id;
 	}
 
-	public Card(GameObject prefab, Transform transform, int id, Vector3 position, Sprite[] cards)
+	public CardType cardType()
+	{
+		return m_cardType;
+	}
+
+	public void setPosition(Vector3 position)
+	{
+		m_button.transform.position = position;
+	}
+
+	public Card(GameObject prefab, Transform transform, int id, Vector3 position, Sprite[] cards, CardType cardType, Deck deck)
 	{
 		m_id = id;
-		m_position = position;
-		m_prefab = prefab;
-		m_transform = transform;
-		m_cards = cards;
+		m_cardType = cardType;
+
+ 		m_button = Instantiate(prefab, transform);
+ 		m_button.transform.position = position;
+ 		m_button.transform.localScale = new Vector3(3f, 3f, 3f);
+ 		// button.GetComponent<Image>().sprite = m_cards[Random.Range(0, m_cards.Length - 1)];
+ 		m_button.GetComponent<Image>().sprite = cards[Card.cardTypeToInt(cardType)];
+ 		// button.GetComponent<Image>().sprite = m_cards[0];
+		// button.GetComponent<Button>().onClick.AddListener(OnClick);
+		m_button.GetComponent<Button>().onClick.AddListener(() => deck.OnCardClick(m_id));
+
 	}
 
 	// Use this for initialization
 	public void Start ()
 	{
- 		GameObject button = Instantiate(m_prefab, m_transform);
- 		button.transform.position = m_position;
- 		button.transform.localScale = new Vector3(3f, 3f, 3f);
- 		button.GetComponent<Image>().sprite = m_cards[Random.Range(0, m_cards.Length - 1)];
-		button.GetComponent<Button>().onClick.AddListener(OnClick);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		
-	}
-
-	void OnClick()
-	{
-		Debug.Log("You have clicked the button!");
 	}
 
 }
--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -2,44 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class Deck : MonoBehaviour
 {
 	public GameObject prefab;
 	public Canvas canvas;
 
-	private Card [][] m_cards;
+	private Vector3 m_selectedCardsBasePosition = new Vector3(100,100,0);
+
+	private List<Card> m_cards;
+	private List<Card> m_selectedCards;
 
 	// Use this for initialization
 	void Start ()
 	{
-		m_cards = new Card[5][];
-		for (int i = 0; i < 5; i++)
-		{
-			m_cards[i] = new Card[5];
-		}
+		createCards();
+	}
+	
+	private void createCards()
+	{
+		m_cards = new List<Card>();
+		m_selectedCards = new List<Card>();
 		
 		Sprite[] cardsImages = Resources.LoadAll<Sprite> ("cards");
 		float absWidth = canvas.GetComponent<RectTransform>().rect.width;
 		float oneAbsW = absWidth / 9; // единица масштабирования ширины - смотреть issue #49
 		float absHeight = canvas.GetComponent<RectTransform>().rect.height;
 		float oneAbsH = absHeight / 16; // единица масштабирования высоты - смотреть issue #49
+		
+		int index = 0;
+
 		for (int i = 0; i < 3; ++i)
 		{
 			for (int j = 0; j < 4; ++j)
 			{
-				Debug.Log("wqewef");
-				m_cards [i] [j] = new Card(prefab, canvas.transform, i*j, new Vector3(
+				m_cards.Add(new Card(prefab, canvas.transform, index, new Vector3(
 					-3f * oneAbsW + j * 2f * oneAbsW, 
 					0.5f * oneAbsH + i * 2f * oneAbsH, 
-					0f), cardsImages
-				);
-				m_cards [i] [j].Start();
+					0f), 
+					cardsImages,
+					Card.CardType.PLUS_2,
+					this
+				));
+				index++;
 			}
 		}
 
 	}
-	
+
+	public void OnCardClick(int index)
+	{
+		Debug.Log("You have clicked the button! " + Convert.ToString(index));
+		Card card = m_cards[index];
+		m_selectedCards.Add(card);
+		card.setPosition(new Vector3(
+					-200 + m_selectedCards.Count * 100, 
+					-300, 
+					0f));
+
+		Debug.Log(Convert.ToString(m_selectedCards.Count));
+	}
+
 	// Update is called once per frame
 	void Update ()
 	{
